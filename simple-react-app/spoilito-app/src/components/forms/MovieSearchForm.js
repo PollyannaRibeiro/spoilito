acb3d87c8ed8f8ad8fe2c7edfa3a4f2960a41995
Pollyanna Ribeiro <23667108+PollyannaRibeiro@users.noreply.github.com>
2022-09-06 22:03:30 +0100

    searchForm checkbox setState

--- simple-react-app/spoilito-app/src/components/forms/MovieSearchForm.js
+++ simple-react-app/spoilito-app/src/components/forms/MovieSearchForm.js
@@ -9,30 +9,48 @@ class ArrayHelper {
     }
 }
 
-const GenreNoPreference = "genre-no-preference";
+const TypeDefault = "type-any";
+const GenreDefault = "genre-no-preference";
+const TriggerDefault =  "trigger-none";
+
 
 class MovieSearchForm extends React.Component {
 
     constructor(props){
         super(props);
         this.state={
-            typeState: "Any",
-            genreState: [GenreNoPreference],
-            triggerState: ["Don't Worry with me", ]
+            typeState: [TypeDefault],
+            genreState: [GenreDefault
+],
+            triggerState: [TriggerDefault ]
 
         };
         
 
         this.handleInputTypeChange = this.handleInputTypeChange.bind(this);
         this.handleInputGenresChange = this.handleInputGenresChange.bind(this);
+        this.handleInputTriggerChange = this.handleInputTriggerChange.bind(this);
+
     }
 
     handleInputTypeChange(event){
-        const target = event.target;
-        const value = target.type === 'checkbox' ? target.checked : target.value;
+        let newTypeArray = [...this.state.typeState]
+
+        if (event.target.checked) {
+            newTypeArray.push(event.target.name);
+        } else {
+            ArrayHelper.remove(event.target.name, newTypeArray);
+        }
+
+        // remove fist element of an array 
+        if (newTypeArray.length > 1) {
+           newTypeArray.shift();
+        } else if (newTypeArray.length == 0) {
+            newTypeArray.push(TypeDefault);
+        }
 
         this.setState({
-            typeState: value
+            typeState: newTypeArray
         });
     }
 
@@ -44,23 +62,40 @@ class MovieSearchForm extends React.Component {
             newArray.push(event.target.name);
         } else {
             ArrayHelper.remove(event.target.name, newArray);
-
-            // let index = newArray.findIndex(function (element){
-            //     return element == event.target.name;
-            // });
-            // newArray.splice(index, 1);
         }
 
-        if (newArray.length > 1 && newArray.includes(GenreNoPreference)) {
-            ArrayHelper.remove(GenreNoPreference, newArray);
+        if (newArray.length > 1 && newArray.includes(GenreDefault)) {
+            ArrayHelper.remove(GenreDefault, newArray);
         } else if (newArray.length == 0) {
-            newArray.push(GenreNoPreference);
+            newArray.push(GenreDefault);
         }
 
         this.setState({
             genreState: newArray
         });
     }
+
+    handleInputTriggerChange(event){
+
+        let newTriggerArray = [...this.state.triggerState]
+
+        if (event.target.checked) {
+            newTriggerArray.push(event.target.name);
+        } else {
+            ArrayHelper.remove(event.target.name, newTriggerArray);
+        }
+
+        if (newTriggerArray.length > 1 && newTriggerArray.includes(TriggerDefault)) {
+            ArrayHelper.remove(TriggerDefault, newTriggerArray);
+        } else if (newTriggerArray.length == 0) {
+            newTriggerArray.push(TriggerDefault);
+        }
+
+        this.setState({
+            triggerState: newTriggerArray
+        });
+    }
+
 
     render(){
         return (
@@ -71,18 +106,26 @@ class MovieSearchForm extends React.Component {
                     <br></br>
                     <label>
                         <input 
-                            type="checkbox choose-type" 
+                            type="checkbox" 
                             name="type-movie"  
-                            checked = {this.state.typeState}
+                            checked = {this.state.typeState.includes("type-movie")}
                             onChange = {this.handleInputTypeChange}/>
                         Movie
                     </label>
                     <label>
-                        <input type="checkbox choose-type" name="type-tv-show" />
+                        <input 
+                            type="checkbox" 
+                            name="type-tv-show" 
+                            checked = {this.state.typeState.includes("type-tv-show")}
+                            onChange = {this.handleInputTypeChange}/>
                         TV show
                     </label>
                     <label>
-                        <input type="checkbox choose-type" name="type-any" />
+                        <input 
+                            type="checkbox" 
+                            name="type-any" 
+                            checked = {this.state.typeState.includes("type-any")}
+                            onChange = {this.handleInputTypeChange}/>
                         Any
                     </label>
 
@@ -90,88 +133,155 @@ class MovieSearchForm extends React.Component {
                     Choose the genres:
                     <br></br>
                     <label>
-                        <input type="checkbox" 
-                        name="genre-no-preference" 
-                        checked={this.state.genreState.includes("genre-no-preference")}
-                        onChange={this.handleInputGenresChange}/>
+                        <input 
+                            type="checkbox" 
+                            name="genre-no-preference" 
+                            checked={this.state.genreState.includes("genre-no-preference")}
+                            onChange={this.handleInputGenresChange}/>
                         No preference
                     </label>
                     <label>
-                        <input type="checkbox" 
-                        name="genre-action" 
-                        checked={this.state.genreState.includes("genre-action")}
-                        onChange={this.handleInputGenresChange}/>
+                        <input 
+                            type="checkbox" 
+                            name="genre-action" 
+                            checked={this.state.genreState.includes("genre-action")}
+                            onChange={this.handleInputGenresChange}/>
                         action
                     </label>
                     <label>
-                        <input type="checkbox" 
-                        name="genre-adventure" 
-                        checked={this.state.genreState.includes("genre-adventure")}
-                        onChange={this.handleInputGenresChange}/>
+                        <input 
+                            type="checkbox" 
+                            name="genre-adventure" 
+                            checked={this.state.genreState.includes("genre-adventure")}
+                            onChange={this.handleInputGenresChange}/>
                         adventure
                     </label>
                     <label>
-                        <input type="checkbox" name="genre-animation" />
+                        <input 
+                            type="checkbox" 
+                            name="genre-animation" 
+                            checked={this.state.genreState.includes("genre-animation")}
+                            onChange={this.handleInputGenresChange}/>
                         animation
                     </label>
                     <label>
-                        <input type="checkbox" name="genre-biography" />
+                        <input 
+                            type="checkbox" 
+                            name="genre-biography" 
+                            checked={this.state.genreState.includes("genre-biography")}
+                            onChange={this.handleInputGenresChange}/>
                         biography
                     </label>
                     <label>
-                        <input type="checkbox" name="genre-comedy" />
+                        <input 
+                            type="checkbox" 
+                            name="genre-comedy" 
+                            checked={this.state.genreState.includes("genre-comedy")}
+                            onChange={this.handleInputGenresChange}/>
                         comedy
                     </label>
                     <label>
-                        <input type="checkbox" name="genre-documentary" />
+                        <input 
+                            type="checkbox" 
+                            name="genre-documentary" 
+                            checked={this.state.genreState.includes("genre-documentary")}
+                            onChange={this.handleInputGenresChange}/>
                         documentary
                     </label>
                     <label>
-                        <input type="checkbox" name="genre-drama" />
+                        <input 
+                            type="checkbox" 
+                            name="genre-drama"  
+                            checked={this.state.genreState.includes("genre-drama")}
+                            onChange={this.handleInputGenresChange}/>
                         drama
                     </label>
                     <label>
-                        <input type="checkbox" name="genre-fantasy" />
+                        <input 
+                            type="checkbox" 
+                            name="genre-fantasy" 
+                            checked={this.state.genreState.includes("genre-fantasy")}
+                            onChange={this.handleInputGenresChange}/>
                         fantasy
                     </label>
                     <label>
-                        <input type="checkbox" name="genre-historical" />
+                        <input 
+                            type="checkbox" 
+                            name="genre-historical" 
+                            checked={this.state.genreState.includes("genre-historical")}
+                            onChange={this.handleInputGenresChange}/>
                         historical
                     </label>
                     <label>
-                        <input type="checkbox" name="genre-horror" />
+                        <input 
+                            type="checkbox" 
+                            name="genre-horror" 
+                            checked={this.state.genreState.includes("genre-horror")}
+                            onChange={this.handleInputGenresChange}/>
                         horror
                     </label>
                     <label>
-                        <input type="checkbox" name="genre-lgbt" />
+                        <input 
+                            type="checkbox" 
+                            name="genre-lgbt" 
+                            checked={this.state.genreState.includes("genre-lgbt")}
+                            onChange={this.handleInputGenresChange}/>
                         lgbt+
                     </label>
                     <label>
-                        <input type="checkbox" name="genre-musical" />
+                        <input 
+                            type="checkbox" 
+                            name="genre-musical" 
+                            checked={this.state.genreState.includes("genre-musical")}
+                            onChange={this.handleInputGenresChange}/>
                         musical
                     </label>
                     <label>
-                        <input type="checkbox" name="genre-mystery" />
+                        <input 
+                            type="checkbox" 
+                            name="genre-mystery" 
+                            checked={this.state.genreState.includes("genre-mystery")}
+                            onChange={this.handleInputGenresChange}/>
                         mystery
                     </label>
                     <label>
-                        <input type="checkbox" name="genre-romance" />
+                        <input 
+                            type="checkbox" 
+                            name="genre-romance" 
+                            checked={this.state.genreState.includes("genre-romance")}
+                            onChange={this.handleInputGenresChange}/>
                         romance
                     </label>
                     <label>
-                        <input type="checkbox" name="genre-sci-fi" />
+                        <input 
+                            type="checkbox" 
+                            name="genre-sci-fi" 
+                            checked={this.state.genreState.includes("genre-sci-fi")}
+                            onChange={this.handleInputGenresChange}/>
                         sci-fi
                     </label>
                     <label>
-                        <input type="checkbox" name="genre-thriller" />
+                        <input 
+                            type="checkbox" 
+                            name="genre-thriller" 
+                            checked={this.state.genreState.includes("genre-thriller")}
+                            onChange={this.handleInputGenresChange}/>
                         thriller
                     </label>
                     <label>
-                        <input type="checkbox" name="genre-war" />
+                        <input 
+                            type="checkbox" 
+                            name="genre-war" 
+                            checked={this.state.genreState.includes("genre-war")}
+                            onChange={this.handleInputGenresChange}/>
                         war
                     </label>
                     <label>
-                        <input type="checkbox" name="genre-western" />
+                        <input 
+                            type="checkbox" 
+                            name="genre-western" 
+                            checked={this.state.genreState.includes("genre-western")}
+                            onChange={this.handleInputGenresChange}/>
                         western
                     </label>
                     
@@ -179,24 +289,44 @@ class MovieSearchForm extends React.Component {
                     I don't want to watch movies that contain the following triggers:
                     <br></br>
                     <label>
-                        <input type="checkbox" name="trigger-None" />
+                        <input 
+                            type="checkbox" 
+                            name="trigger-none" 
+                            checked={this.state.triggerState.includes("trigger-none")}
+                            onChange={this.handleInputTriggerChange}/>
                         Don't Worry with me 
                     </label>
                     <label>
-                        <input type="checkbox" name="trigger-adult" />
+                        <input 
+                            type="checkbox" 
+                            name="trigger-adult" 
+                            checked={this.state.triggerState.includes("trigger-adult")}
+                            onChange={this.handleInputTriggerChange}/>
                         18+
                     </label>
                     <label>
-                        <input type="checkbox" name="trigger-child-abuse" />
+                        <input 
+                            type="checkbox" 
+                            name="trigger-child-abuse" 
+                            checked={this.state.triggerState.includes("trigger-child-abuse")}
+                            onChange={this.handleInputTriggerChange}/>
                         child sex abuse
                     </label>
                     <label>
-                        <input type="checkbox" name="trigger-sexual-assault" />
-                        sexual assault or rape
+                        <input 
+                            type="checkbox" 
+                            name="trigger-sexual-harassment" 
+                            checked={this.state.triggerState.includes("trigger-sexual-harassment")}
+                            onChange={this.handleInputTriggerChange}/>
+                        sexual harassment
                     </label>
                     <label>
-                        <input type="checkbox" name="trigger-sexual-assault" />
-                        rape or sexual assault
+                        <input 
+                            type="checkbox" 
+                            name="trigger-sexual-assault" 
+                            checked={this.state.triggerState.includes("trigger-sexual-assault")}
+                            onChange={this.handleInputTriggerChange}/>
+                        sexual assault or rape
                     </label>
                     
                     <br></br>
